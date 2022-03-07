969cdcea512de6b2572243263530a11238528245
Ryan Cohen <rcohenprogramming@gmail.com>
2022-03-06 19:59:44 -0500

    Update EFI header.

--- libs/hw/efi/efi.h
+++ libs/hw/efi/efi.h
@@ -3,6 +3,8 @@
  *
  * Most of this is taken from the linux EFI header (https://github.com/torvalds/linux/blob/master/include/linux/efi.h)
  * Other parts are taken from the brutal project EFI loader (https://github.com/brutal-org/brutal) and the UEFI specifications 
+ * 
+ * Some pieces of documentation are taken from the UEFI specification document.
  */
 
 #ifndef HW_EFI_H
@@ -14,6 +16,7 @@
 // it is not needed for compilation.
 #define EFIAPI
 
+// EFI return codes
 #define EFI_SUCCESS             0
 #define EFI_ERROR               (1ull << 63)
 #define EFI_LOAD_ERROR          (1  | EFI_ERROR)
@@ -33,7 +36,6 @@
 typedef u64 EfiStatus;
 typedef u8 EfiBool;
 typedef u16 EfiChar16;
-typedef u64 EfiPhysicalAddr;
 typedef void *EfiHandle;
 typedef u64 EfiPhysicalAddress;
 typedef u64 EfiVirtualAddress;
@@ -74,15 +76,15 @@ typedef struct {
 
 struct _EfiSimpleTextOutput;
 
-typedef EfiStatus (*EfiTextReset)(struct _EfiSimpleTextOutput *self, bool extended_verification);
-typedef EfiStatus (*EfiTextString)(struct _EfiSimpleTextOutput *self, EfiChar16 *string);
-typedef EfiStatus (*EfiTextTestString)(struct _EfiSimpleTextOutput *self, EfiChar16 *string);
-typedef EfiStatus (*EfiTextQueryMode)(struct _EfiSimpleTextOutput *self, u32 mode_number, u32 *columns, u32 *rows);
-typedef EfiStatus (*EfiTextSetMode)(struct _EfiSimpleTextOutput *self, u32 mode_number);
-typedef EfiStatus (*EfiTextSetAttribute)(struct _EfiSimpleTextOutput *self, u32 attribute);
-typedef EfiStatus (*EfiTextClearScreen)(struct _EfiSimpleTextOutput *self);
-typedef EfiStatus (*EfiTextSetCursorPosition)(struct _EfiSimpleTextOutput *self, u32 column, u32 row);
-typedef EfiStatus (*EfiTextEnableCursor)(struct _EfiSimpleTextOutput *self, bool visible);
+typedef EfiStatus (EFIAPI *EfiTextReset)(struct _EfiSimpleTextOutput *self, bool extended_verification);
+typedef EfiStatus (EFIAPI *EfiTextString)(struct _EfiSimpleTextOutput *self, EfiChar16 *string);
+typedef EfiStatus (EFIAPI *EfiTextTestString)(struct _EfiSimpleTextOutput *self, EfiChar16 *string);
+typedef EfiStatus (EFIAPI *EfiTextQueryMode)(struct _EfiSimpleTextOutput *self, uptr mode_number, uptr *columns, uptr *rows);
+typedef EfiStatus (EFIAPI *EfiTextSetMode)(struct _EfiSimpleTextOutput *self, uptr mode_number);
+typedef EfiStatus (EFIAPI *EfiTextSetAttribute)(struct _EfiSimpleTextOutput *self, uptr attribute);
+typedef EfiStatus (EFIAPI *EfiTextClearScreen)(struct _EfiSimpleTextOutput *self);
+typedef EfiStatus (EFIAPI *EfiTextSetCursorPosition)(struct _EfiSimpleTextOutput *self, uptr column, uptr row);
+typedef EfiStatus (EFIAPI *EfiTextEnableCursor)(struct _EfiSimpleTextOutput *self, bool visible);
 
 typedef struct {
     i32 max_mode;
@@ -106,6 +108,41 @@ typedef struct _EfiSimpleTextOutput {
     EfiSimpleTextOutputMode *mode;
 } EfiSimpleTextOutput;
 
+typedef struct _EfiDevicePathProtocol {
+    u8 type;
+    u8 sub_type;
+    u8 length[2];
+} EfiDevicePathProtocol;
+
+typedef uptr (EFIAPI *EfiDevicePathUtilsGetDevicePathSize)(const EfiDevicePathProtocol *device_path);
+typedef EfiDevicePathProtocol *(EFIAPI *EfiDevicePathUtilsDupDevicePath)(const EfiDevicePathProtocol *device_path);
+typedef EfiDevicePathProtocol *(EFIAPI *EfiDevicePathUtilsAppendPath)(const EfiDevicePathProtocol *src1, const EfiDevicePathProtocol *src2);
+typedef EfiDevicePathProtocol *(EFIAPI *EfiDevicePathUtilsAppendNode)(const EfiDevicePathProtocol *device_path, const EfiDevicePathProtocol *device_node);
+typedef EfiDevicePathProtocol *(EFIAPI *EfiDevicePathUtilsAppendInstance)(const EfiDevicePathProtocol *device_path, 
+    const EfiDevicePathProtocol *device_path_instance);
+typedef EfiDevicePathProtocol *(EFIAPI *EfiDevicePathUtilsGetNextInstance)(EfiDevicePathProtocol **device_path_instance, uptr *device_path_instance_size);
+typedef bool (EFIAPI *EfiDevicePathUtilsIsMultiInstance)(const EfiDevicePathProtocol *device_path);
+typedef EfiDevicePathProtocol *(EFIAPI *EfiDevicePathUtilsCreateNode)(u8 node_type, u8 node_sub_type, u16 node_length);
+
+typedef struct _EfiDevicePathUtilitiesProtocol {
+    // Returns the size of the specified device path, in bytes.
+    EfiDevicePathUtilsGetDevicePathSize get_device_path_size;
+    // Duplicates a device path structure.
+    EfiDevicePathUtilsDupDevicePath duplicate_device_path;
+    // Appends the device node to the specified device path.
+    EfiDevicePathUtilsAppendPath append_device_path;
+    // Appends the device path to the specified device path.
+    EfiDevicePathUtilsAppendNode append_device_node;
+    // Appends a device path instance to another device path.
+    EfiDevicePathUtilsAppendInstance append_device_path_instance;
+    // Retrieves the next device path instance from a device path data structure.
+    EfiDevicePathUtilsGetNextInstance get_next_device_path_instance;
+    // Returns true if this is a multi-instance device path.
+    EfiDevicePathUtilsIsMultiInstance is_device_path_multi_instance;
+    // Allocates memory for a device node with the specified type and sub type.
+    EfiDevicePathUtilsCreateNode create_device_node;
+} EfiDevicePathUtilitiesProtocol;
+
 typedef struct _EfiMemoryDescriptor {
     u32 type;
     u32 _pad1;
@@ -117,16 +154,16 @@ typedef struct _EfiMemoryDescriptor {
 
 typedef struct {
     EfiMemoryDescriptor *descriptors;
-    u64 entries;
-    u64 descriptor_size;
-    u64 key;
+    uptr entries;
+    uptr descriptor_size;
+    uptr key;
     u32 version;
 } EfiMemoryMap;
 
-typedef EfiStatus (*EfiAllocatePages)(EfiAllocateType type, EfiMemoryType memory_type, u64 pages, EfiPhysicalAddress *memory);
-typedef EfiStatus (*EfiFreePages)(EfiPhysicalAddress memory, u64 pages);
-typedef EfiStatus (*EfiGetMemoryMap)(u64 *memory_map_size, struct _EfiMemoryDescriptor *memory_map, u64 *map_key, u64 *descriptor_size, u32 *descriptor_version);
-typedef EfiStatus (*EfiSetWatchdogTimer)(u64 timeout, u64 code, u64 data_size, EfiChar16 *data);
+typedef EfiStatus (*EfiAllocatePages)(EfiAllocateType type, EfiMemoryType memory_type, uptr pages, EfiPhysicalAddress *memory);
+typedef EfiStatus (*EfiFreePages)(EfiPhysicalAddress memory, uptr pages);
+typedef EfiStatus (*EfiGetMemoryMap)(uptr *memory_map_size, struct _EfiMemoryDescriptor *memory_map, uptr *map_key, uptr *descriptor_size, u32 *descriptor_version);
+typedef EfiStatus (*EfiSetWatchdogTimer)(uptr timeout, u64 code, uptr data_size, EfiChar16 *data);
 
 typedef struct {
     EfiTableHeader header;
@@ -181,15 +218,15 @@ typedef struct {
 	EfiChar16 *firmware_vendor;
 	u32 firmware_revision;
 	EfiHandle console_in_handle;
-	u64 console_in;// simple text input pointer
+	void *console_in;// simple text input pointer
 	EfiHandle console_out_handle;
 	EfiSimpleTextOutput *console_out;
 	EfiHandle std_error_handle;
 	EfiSimpleTextOutput *std_error;
-	u64 runtime_services;   // runtime services pointer
+	void *runtime_services;   // runtime services pointer
 	EfiBootServices *boot_services;
-	u32 num_table_entries;
-	u64 config_table;  // config table pointer
+	uptr num_table_entries;
+	void *config_table;  // config table pointer
 } EfiSystemTable;
 
 typedef EfiStatus (EFIAPI *EfiImageEntryPoint)(EfiHandle handle, EfiSystemTable *st);
